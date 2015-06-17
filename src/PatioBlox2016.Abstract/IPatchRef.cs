91702d4b1f2ffe889b49962496c6f2269123f681
GaryHome <gary.m.armstrong@gmail.com>
2015-06-17 14:17:52 -0500

    Establish Abstract API project

---
+++ src/PatioBlox2016.Abstract/IPatchRef.cs
@@ -0,0 +1,9 @@
+﻿namespace PatioBlox2016.Abstract
+{
+  public interface IPatchRef
+  {
+    string FilePath { get; }
+    string PatchName { get; }
+    int Rowindex { get; }
+  }
+}
---
+++ src/PatioBlox2016.Abstract/IProduct.cs
@@ -0,0 +1,13 @@
+﻿namespace PatioBlox2016.Abstract
+{
+  using System.Collections.Generic;
+
+  public interface IProduct
+  {
+    int Sku { get; }
+    string Color { get; set; }
+    string Size { get; set; }
+    string Name { get; set; }
+    List<string> Descriptions { get; } 
+  }
+}
