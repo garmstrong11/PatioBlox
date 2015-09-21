bd41d43c4d85f56c46c9da0b568dd83fd86fdef2
garyWork2 <garmstrong@integracolor.com>
2015-09-21 14:19:09 -0500

    Add tests for Cell and Page

---
+++ src/PatioBlox2016.Tests/ConcreteTests/CellTests.cs
@@ -0,0 +1,28 @@
+﻿namespace PatioBlox2016.Tests.ConcreteTests
+{
+  using FluentAssertions;
+  using NUnit.Framework;
+  using PatioBlox2016.Concrete;
+
+  [TestFixture]
+  public class CellTests
+  {
+    [Test]
+    public void ToJsxString_ReturnsExpected()
+    {
+      var cell = new Cell(69, 11111, "128", "Descr")
+      {
+        Color = "Color",
+        Upc = "Upc",
+        Size = "Size",
+        Name = "Name"
+      };
+
+      var jsx = cell.ToJsxString(4);
+
+      jsx
+        .Should()
+        .Be("\t\t\t\t{ 'sku' : 11111, 'upc' : 'Upc', 'size' : 'Size', 'color' : 'Color', 'name' : 'Name', 'palletQty' : '128' }");
+    }
+  }
+}
---
+++ src/PatioBlox2016.Tests/ConcreteTests/PageTests.cs
@@ -0,0 +1,36 @@
+﻿namespace PatioBlox2016.Tests.ConcreteTests
+{
+  using System.Collections.Generic;
+  using FluentAssertions;
+  using NUnit.Framework;
+  using PatioBlox2016.Concrete;
+
+  [TestFixture]
+  public class PageTests
+  {
+    [Test]
+    public void ToJsxString_ReturnsCorrect()
+    {
+      var cell1 = new Cell(69, 11111, "128", "Descr")
+      {
+        Color = "Color1",
+        Upc = "Upc1",
+        Size = "Size1",
+        Name = "Name1"
+      };
+
+      var cell2 = new Cell(70, 11112, "129", "Descr2")
+      {
+        Color = "Color2",
+        Upc = "Upc2",
+        Size = "Size2",
+        Name = "Name2"
+      };
+
+      var page = new Page(new Section(new Book(new Job(), "AB"), "Section Name", 19, 4), new List<Cell> {cell1, cell2});
+      var actual = page.ToJsxString(3);
+
+      actual.Should().Be("foo");
+    }
+  }
+}
