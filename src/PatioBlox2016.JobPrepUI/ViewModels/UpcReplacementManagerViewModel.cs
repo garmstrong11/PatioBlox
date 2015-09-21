40a4ba2a8f51eab759a872d2d3a9d470ea51fa32
garyWork2 <garmstrong@integracolor.com>
2015-09-21 14:19:41 -0500

    Testing code for JobFactory

--- src/PatioBlox2016.JobPrepUI/ViewModels/UpcReplacementManagerViewModel.cs
+++ src/PatioBlox2016.JobPrepUI/ViewModels/UpcReplacementManagerViewModel.cs
@@ -40,8 +40,8 @@
 
       //var dupes = products.Where(p => p.HasPatchProductDuplicates);
       
-      var job = _jobFactory.CreateJob();
-      var badBooks = job.Books.Where(b => b.HasDuplicateCells);
+      //var job = _jobFactory.CreateJob();
+      //var badBooks = job.Books.Where(b => b.HasDuplicateCells);
       UpcReplacements.AddRange(products.Select(p => new UpcReplacementViewModel(p)));
       
       base.OnActivate();
